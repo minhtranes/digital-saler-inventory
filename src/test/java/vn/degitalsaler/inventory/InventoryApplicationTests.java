f5b240514cba73b9ccb0e93be0fd9dfbbabd9184
Tran Quang Minh <minh.tran.1@swisspost.com>
2021-04-30 01:13:35 +0700

    initialize the project

---
+++ src/test/java/vn/degitalsaler/inventory/InventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.degitalsaler.inventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
