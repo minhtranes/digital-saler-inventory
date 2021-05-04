ae3040206b6e024b0a577603388c19eea1cc773c
Tran Quang Minh <minh.tran.1@swisspost.com>
2021-05-05 00:54:11 +0700

    add the product service

---
+++ src/main/java/vn/degitalsaler/inventory/application/service/impl/ProductServiceImpl.java
@@ -0,0 +1,17 @@
+/*
+ * Class: ProductServiceImpl
+ *
+ * Created on May 5, 2021
+ *
+ * (c) Copyright Swiss Post Solution, unpublished work
+ * All use, disclosure, and/or reproduction of this material is prohibited
+ * unless authorized in writing.  All Rights Reserved.
+ * Rights in this program belong to:
+ * Swiss Post Solution.
+ * Floor 4-5-8, ICT Tower, Quang Trung Software City
+ */
+package vn.degitalsaler.inventory.application.service.impl;
+
+public class ProductServiceImpl {
+
+}
