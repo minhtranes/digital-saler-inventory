ea3ed94300139046fe61117c9d3c39cd5f6fc654
Tran Quang Minh <minh.tran.1@swisspost.com>
2021-05-05 01:13:51 +0700

    add the representation

---
+++ src/main/java/vn/degitalsaler/inventory/represenation/integrator/ProductProcessor.java
@@ -0,0 +1,17 @@
+/*
+ * Class: ProductIntegrationProcessor
+ *
+ * Created on May 5, 2021
+ *
+ * (c) Copyright Swiss Post Solution, unpublished work
+ * All use, disclosure, and/or reproduction of this material is prohibited
+ * unless authorized in writing.  All Rights Reserved.
+ * Rights in this program belong to:
+ * Swiss Post Solution.
+ * Floor 4-5-8, ICT Tower, Quang Trung Software City
+ */
+package vn.degitalsaler.inventory.represenation.integrator;
+
+public class ProductProcessor {
+
+}
